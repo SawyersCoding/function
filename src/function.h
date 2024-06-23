1d7781c63ec297848832547ed8b4d2670d75fa1f
Sawyer Stanley <sawyerscoding@gmail.com>
2024-06-23 13:10:45 -0300

    Sorted functions lexicographically

--- src/function.h
+++ src/function.h
@@ -6,15 +6,15 @@
 
 class function{
     private:
-        std::string name;
         std::string infix;
+        std::string name;
 
     private:
        function(std::string infix);
 
     public:
-        inline std::string get_name() { return name; }
         inline std::string get_infix() { return infix; } 
+        inline std::string get_name() { return name; }
 
     public:
         static function *try_parse_infix(std::string infix);
