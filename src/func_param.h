b55060b70d674eda8f3d950111160f55168c8516
Sawyer Stanley <sawyerscoding@gmail.com>
2024-06-23 13:16:09 -0300

    Changed func_constant to func_param - works as a variable too

---
+++ src/func_param.h
@@ -0,0 +1,15 @@
+#ifndef FUNC_PARAM
+#define FUNC_PARAM
+
+#include <string>
+
+struct func_param{
+
+    public:
+        std::string symbol;
+        std::string name;
+        double value;
+
+};
+
+#endif // FUNC_PARAM
