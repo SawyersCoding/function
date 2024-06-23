a361e7cb7701ff50dee5827ff4757abc8f448651
Sawyer Stanley <sawyerscoding@gmail.com>
2024-06-23 18:05:02 -0300

    Adding to function declaration

--- src/function.h
+++ src/function.h
@@ -1,20 +1,31 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
-#include <string>
+#include "func_param.h"
 #include <stdexcept>
+#include <string>
+#include <vector>
 
 class function{
     private:
         std::string infix;
         std::string name;
+        std::string rpn;
+        std::vector<func_param> constants;
+        std::vector<func_param> variables;
 
     private:
-       function(std::string infix);
+       function(std::string infix, std::string rpn);
 
     public:
+        double eval(std::vector<double> variable_values);
+        inline func_param *get_constant(int i) { return &constants[i]; }
         inline std::string get_infix() { return infix; } 
         inline std::string get_name() { return name; }
+        inline std::string get_rpn() { return rpn; }
+        inline func_param *get_variable(int i) { return &variables[i]; }
+        inline int nconstants() { return constants.size(); }
+        inline int nvariables() { return variables.size(); }
 
     public:
         static function *try_parse_infix(std::string infix);
