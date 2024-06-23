ec45fbcbaa98a7eb5e5c86849fe77ba4f6174b03
Sawyer Stanley <sawyerscoding@gmail.com>
2024-06-23 19:00:27 -0300

    operate function uses vector of values instead of variadic function

--- src/operator/ioperator.h
+++ src/operator/ioperator.h
@@ -9,7 +9,7 @@ class ioperator{
     public:
         virtual std::string name() = 0;
         virtual bool is_right_associative() = 0;
-        virtual double operate(...) = 0;
+        virtual double operate(std::vector<double> xi) = 0;
         virtual int precedence() = 0;
         virtual std::string symbol() = 0;
 
