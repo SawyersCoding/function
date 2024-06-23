455e88a99a1611ca72670bb3bf6a9fa3ac06a698
Sawyer Stanley <sawyerscoding@gmail.com>
2024-06-23 13:16:34 -0300

    Just making notes

--- src/function.cpp
+++ src/function.cpp
@@ -3,5 +3,6 @@
 function *function::try_parse_infix(std::string infix)
 {
     // Apply shunting yard algorithm here
+    // Must understand what is constant and what is variable
     return nullptr;
 }
