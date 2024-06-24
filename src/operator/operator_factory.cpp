b889b9efb98a8ccd2af9ab3d5e3f8897ac10a1cd
Sawyer Stanley <sawyerscoding@gmail.com>
2024-06-23 21:51:21 -0300

    Created a factory for operators

---
+++ src/operator/operator_factory.cpp
@@ -0,0 +1,26 @@
+#include "operator_factory.h"
+
+operator_factory::operator_factory()
+{
+    map["+"] = new addition_operator();
+    map["/"] = new division_operator();
+    map["*"] = new multiplication_operator();
+    map["-"] = new subtraction_operator();
+}
+
+operator_factory::~operator_factory()
+{
+    delete map["+"]; // addition
+    delete map["/"]; // division
+    delete map["*"]; // multiplication
+    delete map["-"]; // subtraction
+}
+
+ioperator *operator_factory::parse(std::string op)
+{
+    if(map.count(op) == 0){
+        throw operator_argument_exception("The given argument, \"" + op + "\", cannot be parsed into an operator.");
+    }
+
+    return map[op];
+}
---
+++ src/operator/operator_factory.h
@@ -0,0 +1,25 @@
+#ifndef OPERATOR_FACTORY_H
+#define OPERATOR_FACTORY_H
+
+#include "addition_operator.h"
+#include "division_operator.h"
+#include "ioperator.h"
+#include "multiplication_operator.h"
+#include "subtraction_operator.h"
+#include <unordered_map>
+
+class operator_factory{
+
+    private:
+        std::unordered_map<std::string, ioperator*> map;
+
+    public:
+        operator_factory();
+        ~operator_factory();
+
+    public:
+        ioperator *parse(std::string op);
+
+};
+
+#endif // OPERATOR_FACTORY_H
